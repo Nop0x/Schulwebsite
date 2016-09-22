31860d4e3340f754b3a382e6a275ce2067538977
Nop0x <st4lk3r1992@gmail.com>
2016-09-22 16:38:42 +0200

    login fertig

--- admin/admin.php
+++ admin/admin.php
@@ -30,16 +30,13 @@
             if (isset($_COOKIE["username"]))
                 echo "
                   <ul class=\"nav navbar-nav navbar-right\">
-                      <li class=\"active\"><a href=\"/admin\">Admin</a></li>
                       <li class=\"dropdown\">
                         <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" 
                         aria-expanded=\"false\">Welcome " . $_COOKIE["username"] . "<span class=\"caret\"></span></a>
                             <ul class=\"dropdown-menu\">
-                            <li><a href=\"#\">Action</a></li>
-                            <li><a href=\"#\">Another action</a></li>
-                            <li><a href=\"#\">Something else here</a></li>
+                            <li><a href=\"/admin\">Admin</a></li>
                             <li role=\"separator\" class=\"divider\"></li>
-                            <li><a href=\"#\">Separated link</a></li>
+                            <li><a href=\"logout.php\">Logout</a></li>
                             </ul>
                   </ul>";
             else
