e23de79dcb632542a64246a21ef571318f277a2b
Nop0x <st4lk3r1992@gmail.com>
2016-09-23 09:32:42 +0200

    login fertig

--- admin/index.php
+++ admin/index.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Schulwebseite</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/bootstrap-theme.css">
-    <script src="js/jquery-3.1.0.min.js"></script>
-    <script src="js/bootstrap.js"></script>
+    <title>Schulwebseite - Admin</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.css">
+    <script src="../js/jquery-3.1.0.min.js"></script>
+    <script src="../js/bootstrap.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-default">
@@ -33,24 +33,18 @@
                       <li class=\"dropdown\">
                         <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" 
                         aria-expanded=\"false\">Welcome " . $_COOKIE["username"] . "<span class=\"caret\"></span></a>
-                            <ul class=\"dropdown-menu\">
-                            <li><a href=\"/admin\">Admin</a></li>
-                            <li role=\"separator\" class=\"divider\"></li>
-                            <li><a href=\"logout.php\">Logout</a></li>
-                            </ul>
+                        <ul class=\"dropdown-menu\">
+                        <li><a href=\"/admin\">Admin</a></li>
+                        <li role=\"separator\" class=\"divider\"></li>
+                        <li><a href=\"/logout\">Logout</a></li>
+                        </ul>
+                        </li>
                   </ul>";
             else
-                echo "<ul class=\"nav navbar-nav\">
+                echo "<ul class=\"nav navbar-nav navbar-right\">
                         <li><a href=\"/login\">Login</a></li>
-                      </ul>\";";
+                      </ul>";
             ?>
         </div>
     </div>
 </nav>
-<?php
-/**
- * Created by PhpStorm.
- * User: Marvin
- * Date: 22.09.2016
- * Time: 09:45
- */
