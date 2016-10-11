3604033872b67c0f377ce9cc642e2aab7b462f83
Nop0x <st4lk3r1992@gmail.com>
2016-10-11 14:10:55 +0200

    ganz viel stuff

---
+++ roomplan/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Schulwebseite - Admin</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.css">
+    <script src="../js/jquery-3.1.0.min.js"></script>
+    <script src="../js/bootstrap.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <?php
+    require "../php/nav.php";
+    get_nav("room")
+    ?>
+</nav>
+<div class="container-fluid">
+    dein scheiß hier
+</div>
+</body>
