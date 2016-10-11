4fee1f650a7bef28631998bd3906de0d22af1e03
Nop0x <st4lk3r1992@gmail.com>
2016-10-11 14:03:24 +0200

    ganz viel stuff

--- php/nav.php
+++ php/nav.php
@@ -21,20 +21,19 @@ function get_nav($toggle)
                 echo "<li class=\"\"><a href=\"/\">Neuigkeiten <span class=\"sr-only\">(current)</span></a></li>
                 <li class=\"active\"><a href=\"/classes/classesoverview.php\">Stundenplan<span class=\"sr-only\">(current)</span></a></li>";
             echo "</ul>";
-            if (isset($_COOKIE["username"])) {
+            if (isset($_COOKIE["username"]))
                 echo "
-                  <ul class=\"nav navbar - nav navbar - right\">
-                      <li class=\"dropdown\">
-                        <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\"
-                        aria - expanded = \"false\" > Welcome " . $_COOKIE["username"] . "<span class=\"caret\"></span></a>
-                        <ul class=\"dropdown - menu\">
-                        <li><a href=\" / admin\">Admin</a></li>
-                        <li role=\"separator\" class=\"divider\"></li>
-                        <li><a href=\" /logout\">Logout</a></li>
-                        </ul>
-                        </li>
-                  </ul>";
-            }
+                          <ul class=\"nav navbar-nav navbar-right\">
+                              <li class=\"dropdown\">
+                                <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" 
+                                aria-expanded=\"false\">Welcome " . $_COOKIE["username"] . "<span class=\"caret\"></span></a>
+                                <ul class=\"dropdown-menu\">
+                                <li><a href=\"/admin\">Admin</a></li>
+                                <li role=\"separator\" class=\"divider\"></li>
+                                <li><a href=\"/logout\">Logout</a></li>
+                                </ul>
+                                </li>
+                          </ul>";
             else {
                 echo "<ul class=\"nav navbar-nav navbar-right\">
                         <li><a href=\"/login\">Login</a></li>
